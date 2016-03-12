dc44890ff9bc47ed221a18ca859999646b2a7f86
Bob Davies <admin@bobbigmac.com>
2016-03-12 01:44:04 +0000

    Integrate reveal.js for showing slides

--- client/presentation.js
+++ client/presentation.js
@@ -1,7 +1,58 @@
 
 
+Template.slide.helpers({
+	'editMode': function() {
+		return Session.get('editMode');
+	},
+});
+
 Template.presentationTitle.helpers({
 	'momentCreatedAgo': function() {
 		return new moment(this.created).fromNow();
 	}
+});
+
+var revealSettings = {
+  loop: false,
+  controls: true,
+  progress: true,
+  autoSlide: 0, //5000,
+  dependencies: [
+    // Cross-browser shim that fully implements classList - https://github.com/eligrey/classList.js/
+    //{ src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+
+    // Interpret Markdown in <section> elements
+    { src: 'client-libs/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+    { src: 'client-libs/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+
+    // Syntax highlight for <code> elements
+    //{ src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+
+    // Speaker notes
+    //{ src: 'plugin/notes/notes.js', async: true },
+	]
+};
+
+Template.presentation.onRendered(function() {
+	//See https://github.com/hakimel/reveal.js#instructions
+	Reveal.initialize(revealSettings);
+
+	var refreshMarkdown = function() {
+		if(typeof RevealMarkdown != 'undefined') {
+			window.setTimeout(function() {
+				RevealMarkdown.initialize(revealSettings);
+			}, 10);
+		}
+	};
+
+	Slides.find().observeChanges({
+		added: refreshMarkdown,
+		changed: refreshMarkdown,
+		removed: refreshMarkdown
+	});
+
+	Reveal.addEventListener('slidechanged', function(event) {
+		//TODO: Update any remotes
+		//console.log('slide changed', event);
+	});
 });
--- shared/prep/router.js
+++ shared/prep/router.js
@@ -40,12 +40,12 @@ Router.route('/:presentation', {
 		var slides = Slides.find({ presentation: this.params.presentation }, { sort: { created: -1 }});
 
 		Session.set('page-title', slides.count()+' slides: ' + (presentation && presentation.title));
-		//slides = slides.fetch();
+		slides = slides.fetch();
 
 		//Default/new slide
 		var newSlide = { content: 'New slide', presentation: presentation && presentation._id, owner: Meteor.userId() };
-		//slides.push(newSlide);
+		slides.push(newSlide);
 		
-		return { presentation: presentation, slides: slides, extra: newSlide };
+		return { presentation: presentation, slides: slides };
 	}
 });
