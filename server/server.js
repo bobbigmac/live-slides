36b3381143394f1239b43742fe30f47a20ba4b7a
Bob Davies <admin@bobbigmac.com>
2016-03-14 19:34:31 +0000

    Allow toggling of guest users via method (demo-fudge)

--- server/server.js
+++ server/server.js
@@ -2,5 +2,19 @@
 Meteor.startup(function () {
 	if(typeof AccountsGuest !== 'undefined') {
 		AccountsGuest.name = true;
+		AccountsGuest.forced = false;
 	}
+
+	Meteor.methods({
+		'enable-guest-accounts': function() {
+			if(typeof AccountsGuest !== 'undefined') {
+				AccountsGuest.enabled = true;
+			}
+		},
+		'disable-guest-accounts': function() {
+			if(typeof AccountsGuest !== 'undefined') {
+				AccountsGuest.enabled = false;
+			}
+		}
+	});
 });
